395c5bcc9bd25898fee8717a362999ee14aa11a9
Nicolas Bissig <nicbissig@gmail.com>
2021-09-13 18:10:34 +0200

    Add basic plugin structure

---
+++ src/main/java/net/ddns/mrtiptap/servermetrics/ServerMetrics.java
@@ -0,0 +1,17 @@
+package net.ddns.mrtiptap.servermetrics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServerMetrics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
