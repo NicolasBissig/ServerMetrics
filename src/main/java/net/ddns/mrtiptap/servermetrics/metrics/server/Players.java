24efaa9cb3fe28b1fb809899b723c1c2f161d3ec
Nicolas Bissig <nicbissig@gmail.com>
2021-10-06 21:52:55 +0200

    Add metric for single player online time

--- src/main/java/net/ddns/mrtiptap/servermetrics/metrics/server/Players.java
+++ src/main/java/net/ddns/mrtiptap/servermetrics/metrics/server/Players.java
@@ -2,16 +2,27 @@ package net.ddns.mrtiptap.servermetrics.metrics.server;
 
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import net.ddns.mrtiptap.servermetrics.metrics.listeners.NewPlayerJoinListener;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import java.util.Objects;
 
 public class Players extends MinecraftServerBinder {
+    private MeterRegistry registry;
+
     public Players(Plugin plugin, ConfigurationSection configuration) {
         super(plugin, configuration);
+
+        new NewPlayerJoinListener(getPlugin(), this::registerGaugeForPlayer);
     }
 
     @Override
-    public void bindTo(MeterRegistry registry) {
+    public void bindTo(@NotNull MeterRegistry registry) {
+        this.registry = registry;
+
         Gauge.builder("minecraft.players.total", () -> getMinecraftServer().getOfflinePlayers().length)
             .description("Amount of unique players that have joined the server")
             .register(registry);
@@ -29,6 +40,10 @@ public class Players extends MinecraftServerBinder {
         Gauge.builder("minecraft.players.fullness.percent", this::calculateServerFullness)
             .description("Fullness of the server, between 0 (empty) and 1.0 (full)")
             .register(registry);
+
+        for (OfflinePlayer player : getMinecraftServer().getOfflinePlayers()) {
+            registerGaugeForPlayer(player);
+        }
     }
 
     private double calculateServerFullness() {
@@ -36,5 +51,17 @@ public class Players extends MinecraftServerBinder {
         final double capacity = getMinecraftServer().getMaxPlayers();
 
         return online / capacity;
+    }
+
+    private void registerGaugeForPlayer(OfflinePlayer player) {
+        final String playerName = Objects.requireNonNullElse(player.getName(), "");
+        final String UUID = player.getUniqueId().toString();
+        final Tags tags = Tags.of("name", playerName, "UUID", UUID);
+
+        Gauge.builder("minecraft.player.online",
+                () -> player.isOnline()? 1 : 0)
+            .tags(tags)
+            .description("Online state of a player")
+            .register(registry);
     }
 }
